5edcc4987814180aaecd2917e0da8b707f8f0325
Ken Koch <kkoch986@gmail.com>
2020-02-07 15:28:28 -0500

    remove the old "blink" sketch
    Now that we have some real code coming in, no need to keep that.

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,9 @@
 #include <lighting.h>
 #endif
 
-/**
- * Just implements a basic "blink" sketch.
- * on the esp32 dev board pin D2 controls the on-board LED.
- */
-#define LED_PIN IN_LED_PIN
-
 void setup() {
 #if defined ENABLE_LIGHTING
   lighting_setup();
-#else   
-  pinMode(LED_PIN, OUTPUT);
 #endif
 }
 
@@ -23,10 +15,5 @@ void loop() {
 
 #if defined ENABLE_LIGHTING
   lighting_loop();
-#else 
-  delay(1000);
-  digitalWrite(LED_PIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_PIN, LOW);
 #endif
 }
