8e114bd121551cb10308ebb55c652021ba49d319
Bryan Grunow <bryan@linqage.com>
2020-02-07 10:29:05 -0500

    Added lighting lib and some initial build flags

---
+++ lib/lighting/src/lighting.h
@@ -0,0 +1,6 @@
+#include <Arduino.h>
+#include <FastLED.h>
+
+void lighting_setup();
+
+void lighting_loop();
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include <Arduino.h>
 
+//
+#if defined ENABLE_LIGHTING
+#include <lighting.h>
+#endif
+
 /**
  * Just implements a basic "blink" sketch.
  * on the esp32 dev board pin D2 controls the on-board LED.
@@ -7,12 +12,21 @@
 #define LED_PIN IN_LED_PIN
 
 void setup() {
+#if defined ENABLE_LIGHTING
+  lighting_setup();
+#else   
   pinMode(LED_PIN, OUTPUT);
+#endif
 }
 
 void loop() {
+
+#if defined ENABLE_LIGHTING
+  lighting_loop();
+#else 
   delay(1000);
   digitalWrite(LED_PIN, HIGH);
   delay(1000);
   digitalWrite(LED_PIN, LOW);
+#endif
 }
